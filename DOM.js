c212f2ed080eeaad99cfd6859c58f524be27ebd3
Abdul Ahmad <abdulfatah.a.ahmad@gmx.com>
2019-07-16 12:32:33 -0400

    Added functionality to the form

--- DOM.js
+++ DOM.js
@@ -1,2 +1,110 @@
+// Examine document and window
 console.dir(document); 
 console.log("❓");
+console.log(document);
+console.log(Window);
+console.log(window);
+
+
+// console.log(document.getElementById('header-title').textContent = "Hello");
+//console.log(document.getElementById('header-title').innerText = "Hi");
+
+// innerText is aware of styling and wont return any hidden text
+
+//console.log(document.getElementById('header-title').innerHTML);
+
+const headerTitle = document.getElementById('header-title');
+headerTitle.style.border = 'solid 3px #000';
+headerTitle.style.backgroundColor = 'red';
+headerTitle.style.display = 'none';
+headerTitle.style.visibility = "hidden"; 
+EXAMINE THE DOCUMENT OBJECT //
+
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+//document.title =  123;
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+// document.all[10].textContent = 'Hello';
+console.log(document.forms[0]);
+console.log(document.links);
+console.log(document.images);
+
+
+console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'Goodbye';
+console.log(headerTitle.innerText);
+headerTitle.innerHTML = '<h3>Hello</h3>';
+header.style.borderBottom = 'solid 3px #000';
+
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < items.length; i++){
+  items[i].style.backgroundColor = '#f4f4f4';
+}
+
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+// Gives error
+//items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < li.length; i++){
+  li[i].style.backgroundColor = '#f4f4f4';
+}
+
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = 'Hello World'
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value="SEND"
+
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even= document.querySelectorAll('li:nth-child(even)');
+
+for(var i = 0; i < odd.length; i++){
+  odd[i].style.backgroundColor = '#f4f4f4';
+  even[i].style.backgroundColor = '#ccc';
+}
+
